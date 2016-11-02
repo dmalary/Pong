d00aec8d6766e9b1e52ccf90b92d00864b2ad6dd
David <davidmalary@gmail.com>
2016-11-02 13:56:35 -0400

    js work

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Sim Sim Salabim');
+// console.log('Sim Sim Salabim');
+
+// requestAnimationFrame
+var animate =   window.requestAnimationFrame ||
+                window.webkitrequestAnimationFrame ||
+                window.mozrequestAnimationFrame ||
+                function(callback) {
+                  window.setTimeout(callback, 1000/60)
+                };
+// creates 60fps and method optimizes browser performance
+
+// Game canvas
+var canvas = document.createElement('canvas');
+var width = 700;
+var height = 400;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+
+window.onload = function() {
+  document.body.appendChild(canvas);
+  animate(setup)
+};
+
+// Game setup
+var setup = function() {
+  update();
+  render();
+  animate(setup);
+}
