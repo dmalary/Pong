d133c6fb86ca05876696b2d994ac41a85c4144ce
David <davidmalary@gmail.com>
2016-11-03 15:15:21 -0400

    ball bounce work

--- app.js
+++ app.js
@@ -106,3 +106,48 @@ Ball.prototype.update = function () {
   this.x += this.xSpeed;
   this.y += this.ySpeed;
 };
+
+var update = function () {
+  ball.update(player.padde, computer.paddle);
+};
+
+Ball.prototype.update = function(paddle1, paddle2) {
+  this.x += this.xSpeed;
+  this.y += this.ySpeed;
+  var leftX = this.x - 5;
+  var leftY = this.y - 5;
+  var rightX = this.x + 5;
+  var rightY = this. y + 5;
+
+  if (this.y - 5 < 0) { // HITTING TOP WALL
+    this.y = 5;
+    this.ySpeed = -this.ySpeed;
+  } else if (this.y + 5 > 400) { // HITTING BOTTOM WALL
+    this.y = 395;
+    this.ySpeed = -this.ySpeed;
+  };
+
+  // POINT SCORED
+  if (this.x < 0 || this.x > 700) {
+    this.xSpeed = 3;
+    this.ySpeed = 0;
+    this.x = 350;
+    this.y = 200;
+  };
+
+  // HIT PLAYER PADDLE
+  if (leftX > 350) {
+    if (leftX < (paddle1.x + paddle1.width) && rightX > paddle1.x && leftY < (paddle1.y + paddle1.height) && rightY > paddle1.y) {
+      this.xSpeed = -3;
+      this.ySpeed += (paddle1.ySpeed / 2);
+      this.x += this.xSpeed;
+    };
+  } else {
+  // HIT COMPUTER PADDLE
+    if (leftX < (paddle2.x + paddle2.width) && rightX > paddle2.x && leftY < (paddle2.y + paddle2.height) && rightY > paddle2.y) {
+      this.xSpeed = 3;
+      this.ySpeed += (paddle2.ySpeed / 2);
+      this.x += this.xSpeed;
+    };
+  }
+};
